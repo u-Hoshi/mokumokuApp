219ad914552c77b0f871f27a57b4e2c4b2ba7b08
u-Hoshi <yuta000707@yahoo.co.jp>
2021-05-26 21:51:44 +0900

    firebaseの導入

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAUJwz-7zU6Y4tokCIlBG0ucLeEd6cNc48',
+  authDomain: 'mokumokuapp-54acc.firebaseapp.com',
+  projectId: 'mokumokuapp-54acc',
+  storageBucket: 'mokumokuapp-54acc.appspot.com',
+  messagingSenderId: '749423443644',
+  appId: '1:749423443644:web:51aff7bcb70daf6d509ab9',
+  measurementId: 'G-S7VL459J5F',
+};
