ed46fc7ba494a4e7ffb42340751120cff16b2f38
u-Hoshi <yuta000707@yahoo.co.jp>
2021-05-26 23:52:52 +0900

    react周りの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { VFC } from 'react'
+import React from 'react'
+// import React from 'react-dom/node_modules/@types/react'
 
-const App = (): VFC => {
-  return <></>
+const App = () => {
+  console.log('hoge')
+  return (
+    <>
+      <p>hoge</p>
+      <h3>hoge</h3>
+    </>
+  )
 }
 
 export default App
