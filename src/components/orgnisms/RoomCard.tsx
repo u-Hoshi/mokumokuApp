87874707bef117d53c91353dff31066f5dccbf21
u-Hoshi <yuta000707@yahoo.co.jp>
2021-05-30 16:33:22 +0900

    月のバグを調整

--- src/components/orgnisms/RoomCard.tsx
+++ src/components/orgnisms/RoomCard.tsx
@@ -26,9 +26,9 @@ const RoomCard: VFC<room> = (props) => {
             title="もくもく会"
             description="主催者:hoge"
           />
-          {console.log(endTimeDT)}
-          <h3>{`${startTimeDT[1]}月${startTimeDT[2]}日${startTimeDT[3]}時${startTimeDT[4]}分から`}</h3>
-          <h3>{`${endTimeDT[1]}月${endTimeDT[2]}日${endTimeDT[3]}時${endTimeDT[4]}分まで`}</h3>
+          {console.log(endTimeDT[1])}
+          <h3>{`${startTimeDT[1] + 1}月${startTimeDT[2]}日${startTimeDT[3]}時${startTimeDT[4]}分から`}</h3>
+          <h3>{`${endTimeDT[1] + 1}月${endTimeDT[2]}日${endTimeDT[3]}時${endTimeDT[4]}分まで`}</h3>
           <h4>{meettype}</h4>
           <h4>{message}</h4>
         </Card>
