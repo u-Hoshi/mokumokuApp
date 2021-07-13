c43c69984f142d29cde34c3279419979518160bf
u-Hoshi <yuta000707@yahoo.co.jp>
2021-07-13 18:20:28 +0900

    ログアウトの位置を変更

--- src/components/pages/Room.tsx
+++ src/components/pages/Room.tsx
@@ -2,7 +2,6 @@ import { Col, Row } from 'antd'
 import RoomDetail from 'components/molecules/RoomDetail'
 import HeaderLayout from 'components/themplates/HeaderLayout'
 import RoomTable from 'components/themplates/RoomTable'
-import { auth } from '../../firebase/index'
 import { VFC, useContext } from 'react'
 import { LoginUserContext } from 'components/providers/LoginUserProvider'
 
@@ -18,8 +17,6 @@ const Room: VFC = () => {
         </Col>
         <Col span={6} offset={1}>
           <RoomDetail user={loginUser} />
-          {/* 仮ログアウトボタン */}
-          <button onClick={() => auth.signOut()}>Logout(仮)</button>
         </Col>
       </Row>
     </>
