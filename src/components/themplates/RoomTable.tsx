a1effaca13a7be951eb08f92dfce118091b1b0fe
u-Hoshi <yuta000707@yahoo.co.jp>
2021-08-17 20:51:56 +0900

    開催日に近い順に並び替えれるように実装

--- src/components/themplates/RoomTable.tsx
+++ src/components/themplates/RoomTable.tsx
@@ -1,50 +1,101 @@
 import RoomCard from 'components/orgnisms/RoomCard'
 import { db } from '../../firebase/index'
 import { useEffect, useState, VFC } from 'react'
-import { Col, Row, Switch } from 'antd'
+import { Col, Row, Switch, Typography } from 'antd'
 import RoomCalendar from 'components/orgnisms/RoomCalendar'
+import moment from 'moment'
+
+const { Text } = Typography
 
 const RoomTable: VFC = () => {
   type room = {
     id: string
     AuthorId: string
-    hostDay: []
-    endTime: []
+    hostDay: number[]
+    endTime: number[]
     meetType: string
     message: string
-    startTime: []
+    startTime: number[]
   }
   // TODO anyを取り除く
   const [rooms, setRooms] = useState<any>([])
 
-  // 全ルームの情報を取得取得
-  useEffect(() => {
-    db.collection('Group1')
-      .orderBy('createdAt', 'desc')
-      .onSnapshot((snapshot) => {
-        const rooms = snapshot.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          }
-        })
-        console.log(rooms)
-        // ↑では[{}]だが↓では[object object]の形で代入される
-        setRooms(rooms)
-      })
-  }, [])
+  const nowTimeInt =
+    moment().toArray()[0] * 100000000 +
+    moment().toArray()[1] * 1000000 +
+    moment().toArray()[2] * 10000 +
+    moment().toArray()[3] * 100 +
+    moment().toArray()[4]
 
   const [isDisplay, setIsDisplay] = useState(false)
-
-  const onChangeSwitch = () => {
+  const onChangeDisplay = () => {
     setIsDisplay(!isDisplay)
   }
+
+  const [cardSort, setCardSort] = useState(true)
+
+  const onChangeSort = () => {
+    setCardSort(!cardSort)
+  }
+
+  // 全ルームの情報を取得取得
+  useEffect(() => {
+    if (cardSort) {
+      db.collection('Group1')
+        .orderBy('startDayTimeInt', 'asc')
+        .where('startDayTimeInt', '>', nowTimeInt)
+        .onSnapshot((snapshot) => {
+          const rooms = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            }
+          })
+          console.log(rooms)
+          // ↑では[{}]だが↓では[object object]の形で代入される
+          setRooms(rooms)
+        })
+    } else {
+      db.collection('Group1')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot((snapshot) => {
+          const rooms = snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            }
+          })
+          console.log(rooms)
+          // ↑では[{}]だが↓では[object object]の形で代入される
+          setRooms(rooms)
+        })
+    }
+  }, [cardSort])
 
   return (
     <>
       <Col offset={1} span={14} style={{ paddingBottom: '14px' }}>
-        表示切り替え：
-        <Switch size="default" checkedChildren="カレンダー" unCheckedChildren="カード" onChange={onChangeSwitch} />
+        <Text>表示切り替え：</Text>
+        <Switch
+          style={{ display: 'inline' }}
+          checkedChildren="カレンダー"
+          unCheckedChildren="カード"
+          onChange={onChangeDisplay}
+        />
+        {isDisplay || (
+          <>
+            <Text style={{ paddingLeft: '14px' }}>カードの並び順：</Text>
+            <Switch
+              defaultChecked
+              checkedChildren="開催日に近い順"
+              unCheckedChildren="作成日順"
+              onChange={onChangeSort}
+              style={{
+                display: 'inline',
+              }}
+            />
+          </>
+        )}
       </Col>
       {isDisplay ? (
         <RoomCalendar rooms={rooms} />
